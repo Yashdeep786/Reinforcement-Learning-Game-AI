357fa7c453c203934aef199b9703ead036f1e4f9
Rajdeep Saxena <rajdeep.saxena115163@marwadiuniversity.ac.ingit config --listgit config --global user.email rajdeepsaxena2002@gmail.com>
2025-06-15 00:38:47 +0530

    Added DBHandler class

--- database/db_handler.py
+++ database/db_handler.py
@@ -2,19 +2,29 @@ import sqlite3
 
 DB_NAME = "game_ai.db"
 
-def save_vocab(vocab):
-    conn = sqlite3.connect(DB_NAME)
-    c = conn.cursor()
-    c.execute("DELETE FROM signal_vocab")  # clear old
-    for signal, meaning in vocab.items():
-        c.execute("INSERT INTO signal_vocab (signal, meaning) VALUES (?, ?)", (signal, meaning))
-    conn.commit()
-    conn.close()
+class DBHandler:
+    def __init__(self):
+        self.conn = sqlite3.connect(DB_NAME, check_same_thread=False)
+        self.cursor = self.conn.cursor()
+        self._init_table()
 
-def load_vocab():
-    conn = sqlite3.connect(DB_NAME)
-    c = conn.cursor()
-    c.execute("SELECT signal, meaning FROM signal_vocab")
-    rows = c.fetchall()
-    conn.close()
-    return {signal: meaning for signal, meaning in rows}
+    def _init_table(self):
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS game_logs (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_signal TEXT,
+                ai_response TEXT,
+                reward INTEGER
+            )
+        ''')
+        self.conn.commit()
+
+    def log(self, user_signal, ai_response, reward):
+        self.cursor.execute(
+            "INSERT INTO game_logs (user_signal, ai_response, reward) VALUES (?, ?, ?)",
+            (user_signal, ai_response, reward)
+        )
+        self.conn.commit()
+
+    def close(self):
+        self.conn.close()
